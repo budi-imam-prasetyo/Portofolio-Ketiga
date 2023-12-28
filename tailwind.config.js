e753503b1d691afea292b2b2d1c23b0908380379
budi-imam-prasetyo <111475648+budi-imam-prasetyo@users.noreply.github.com>
2023-12-29 00:42:46 +0700

    Portofolio V3

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  darkMode: "class",
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#0ea5e9",
+        light: "#e0f2fe",
+        slate: {
+          850: '#182637'
+        }
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
